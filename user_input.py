25ebc47f7378fc9915a36bae92fc9cd7fc3e40f4
Lucidarr <97lucymaina@gmail.com>
2024-03-05 18:38:27 +0300

    print final list

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# ask user for their name
+name =input("enter your name: ")
+
+# ask user for their age
+age=input("enter your age: ")
+
+#ask user for their location
+location=input("Enter your location: ")
+
+#print personalized message
+print( f"Hello {name}, you are {age} years old and live in{location}")
